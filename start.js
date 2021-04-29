41f67a03d2b8a5039dc7e67c50400add2ac92496
Joel Peltonen <joel.peltonen@gmail.com>
2021-04-29 10:03:21 +0200

    Add solution

--- start.js
+++ start.js
@@ -15,3 +15,43 @@ const userDataDump = [
   "D,Cheesedude51,1951,Users",
   "E,cron,,System",
 ];
+
+function getInitialsFromFullName(fullName) {
+  const partsOfNameArray = fullName.split(" ");
+  let initials = "";
+
+  for (let i = 0; i < partsOfNameArray.length; i++) {
+    const partOfName = partsOfNameArray[i];
+    initials = initials + partOfName[0];
+  }
+
+  return initials;
+}
+
+function getDepartmentsFromData(departmentsString) {
+  return departmentsString.split("|");
+}
+
+function convertUserCSVsToObjectsArray(data) {
+  const arrayOfUsers = [];
+
+  for (let i = 0; i < data.length; i++) {
+    // "i" is the index number ot the data
+    const userDataString = data[i]; // B,Robert Smith,,Users|Admin|Sudo
+    const userDataArray = userDataString.split(","); // for example [ 'D', 'Cheesedude51', '1951', 'Users' ]
+
+    const id = userDataArray[0];
+    const fullName = userDataArray[1];
+    const initials = getInitialsFromFullName(fullName);
+    const birthYear = userDataArray[2] ? userDataArray[2] : "Unknown";
+    const departments = getDepartmentsFromData(userDataArray[3]);
+
+    const userObject = { id, fullName, initials, birthYear, departments };
+    arrayOfUsers.push(userObject);
+  }
+
+  return arrayOfUsers;
+}
+
+const result = convertUserCSVsToObjectsArray(userDataDump);
+console.log(result);
